5849000ca8e77cadbe84453d909c0377ea0203f5
Peter <mrpeterssyt@gmail.com>
2024-11-10 12:32:43 -0500

    Initial commit

---
+++ candid-ios/candid_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  candid_iosApp.swift
+//  candid-ios
+//
+//  Created by Peter Bidoshi on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct candid_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ candid-iosTests/candid_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  candid_iosTests.swift
+//  candid-iosTests
+//
+//  Created by Peter Bidoshi on 11/10/24.
+//
+
+import Testing
+@testable import candid_ios
+
+struct candid_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
